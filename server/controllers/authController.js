a0efe4c6b8d8b8204f9f44e50624993b22cc2cca
kiddocoder <ndayikezatresor9@gmail.com>
2024-08-20 00:51:39 +0200

    adding;

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,7 +13,7 @@ const register = async (req, res) => {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Hash password
+    // Hash password;
     const hashedPassword = await hash(password, 10);
 
     // Create new user
